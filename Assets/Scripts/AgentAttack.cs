399d64c570785dc4737de935abb7dee2bb11d925
shivanshudhamija01 <shivanshud@alantrixgeim.com>
2025-11-05 18:33:01 +0530

    Enemy Animations and Basic enemy movement

---
+++ Assets/Scripts/AgentAttack.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentAttack : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Here i need to write a code such that raycast is always towards the player 
+        
+    }
+}
--- Assets/Scripts/AgentMovement.cs
+++ Assets/Scripts/AgentMovement.cs
@@ -1,32 +1,44 @@
 using UnityEngine;
 using UnityEngine.AI;
+using System.Collections;
 
 public class AgentMovement : MonoBehaviour
 {
     [SerializeField] private Animator animator;
+    [SerializeField] private float timeGapBetweenAttacks;
     private Vector3 target;
+    private const string distanceToPlayer = "DistanceToPlayer";
     NavMeshAgent agent;
-
+    private float animatorParameterX;
+    private float animatorParameterY;
+    private float timeSincePreviousAttack = 0f;
     void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
-        // agent.updateRotation = false;
-        // agent.updateUpAxis = false;
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
     }
-
+    void Start()
+    {
+        StartCoroutine(UpdatePathDistance());
+    }
     void Update()
     {
         SetTargetPosition();
         SetAgentPosition();
 
         ForwardDirectionForAnimationSwitching();
+        // CalculateDistanceToTarget();
+        // Also try to align the raycast in the target direction so that 
     }
 
     void SetTargetPosition()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            // target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 clickPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            target = new Vector3(clickPos.x, clickPos.y, 0f);
         }
     }
 
@@ -37,11 +49,47 @@ public class AgentMovement : MonoBehaviour
 
     void ForwardDirectionForAnimationSwitching()
     {
-        Debug.Log("Forward vector is : "+ transform.forward);
-        float x = transform.forward.x;
-        float y = transform.forward.y;
+        Vector3 differenceVector = target - transform.position;
+        Vector3 normalizedVector = differenceVector.normalized;
+        animator.SetFloat("x",normalizedVector.x);
+        animator.SetFloat("y",normalizedVector.y);
+        animatorParameterX = normalizedVector.x;
+        animatorParameterY = normalizedVector.y;
+    }
+    IEnumerator UpdatePathDistance()
+    {
+        while (true)
+        {
+            CalculateDistanceToTarget();
+            yield return new WaitForSeconds(0.2f); 
+        }
+    }
 
-        animator.SetFloat("x",x);
-        animator.SetFloat("y",y);
+
+    void CalculateDistanceToTarget()
+    {
+        NavMeshPath path = new NavMeshPath();
+        // Instead of target use target.position 
+        if (NavMesh.CalculatePath(agent.transform.position, target, NavMesh.AllAreas, path))
+        {
+            float dist = 0f;
+            for (int i = 1; i < path.corners.Length; i++)
+                dist += Vector2.Distance(path.corners[i - 1], path.corners[i]);
+            
+            if(dist > 5f)
+            {
+                animator.SetFloat(distanceToPlayer,0f);
+            }
+            else if(dist <= 5f && dist > 2f)
+            {
+                animator.SetFloat(distanceToPlayer,0.5f);
+            }
+            else 
+            {
+                animator.SetFloat(distanceToPlayer,1f);
+            }
+        }
     }
 }
+
+
